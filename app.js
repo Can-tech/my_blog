21a215ae8a47b4d20ec216e9b71b3e46b3127c30
Ogulcan <eskimasal@outlook.com>
2022-06-24 17:36:41 +0300

    hw8

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog decription"
-    }
-    res.send(blog);
-})
+//Middleware
+app.use(express.static('public'));
+
+
+//Template Engine
+app.set("view engine","ejs");
+
+//Route
+app.get('/', (req, res) => {
+
+  res.render('index');
+
+});
+app.get('/about', (req, res) => {
+
+    res.render('about');
+  
+  });
+  app.get('/add_post', (req, res) => {
+
+    res.render('add_post');
+  
+  });
 
 const port = 5000;
 
-app.listen(port, ()=>{
-    console.log(`server ${port} başlatıldı`);
-})
+app.listen(port, () => {
+  console.log(`server ${port} başlatıldı`);
+});
