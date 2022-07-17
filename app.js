c6be1a3cf4a9774a683b211681c4be69848afb7e
Ogulcan <eskimasal@outlook.com>
2022-07-17 19:17:23 +0300

    DB-Connected

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const Blog = require('./models/Blog');
 const path = require('path');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
+//connect-DB
+mongoose.connect('mongodb://localhost/clean-blog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 //Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 
 //Template Engine
 app.set("view engine","ejs");
 
 //Route
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
 
-  res.render('index');
+  const blog = await Blog.find({});
+  res.render('index', {
+    blog
+  });
 
 });
 app.get('/about', (req, res) => {
@@ -26,6 +39,14 @@ app.get('/about', (req, res) => {
     res.render('add_post');
   
   });
+  app.post('/blog', async (req, res)=>{
+
+    await Blog.create(req.body);
+    res.redirect('/');
+
+  });
+
+
 
 const port = 5000;
 
