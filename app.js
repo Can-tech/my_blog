db17dd0b41c0b3493c2aa657b93181cdc63199ee
Ogulcan <eskimasal@outlook.com>
2022-08-08 13:29:54 +0300

    commit

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ app.get('/add_post', pageController.getAddPost);
 
 
 
-const port = 5000;
+const port = process.env.PORT || 5000
+
 
 app.listen(port, () => {
   console.log(`server ${port} başlatıldı`);
